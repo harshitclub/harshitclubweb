a1ce3a83d8672b7b5f7540721880b50f2df2fdf8
harshitclub <harshitclub@gmail.com>
2023-04-28 23:34:59 +0530

    changes

--- app/components/Hero/Hero.jsx
+++ app/components/Hero/Hero.jsx
@@ -2,21 +2,18 @@ import React from "react";
 import "./style.css";
 import Link from "next/link";
 import { BsDownload } from "react-icons/bs";
-import { RiArrowDownSLine, RiExternalLinkLine } from "react-icons/ri";
+import { RiExternalLinkLine } from "react-icons/ri";
 
 export default function Hero() {
   return (
     <section className="hero flex flexColumn spaceBtw width100">
       <div className="heroContainer flex flexColumn maxWidth width90 alignStart">
         <h1>Harshit Kumar Web Developer</h1>
-        <h2 className="margin1">
+        <h2 className="margin2">
           Hey There, I'm <span>Harshit Kumar</span>, web developer who creates
           dynamic real world websites and web applications.
         </h2>
-        <p className="margin2">
-          <span>JavaScript</span> | <span>React.js</span> | <span>Node.js</span>{" "}
-          | <span>Next.js</span> | <span>More...</span>
-        </p>
+
         <div className="heroBtns flex">
           <div className="heroBtn">
             <Link href="mailto:harshitclub@gmail.com">
@@ -31,9 +28,6 @@ export default function Hero() {
             </Link>
           </div>
         </div>
-      </div>
-      <div className="heroArrow flex alignEnd">
-        <RiArrowDownSLine className="hero-arrow" />
       </div>
     </section>
   );
