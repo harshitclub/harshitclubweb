afec84ef2e9ca43d78fe8778a52b61004a09197c
harshitclub <harshitclub@gmail.com>
2023-04-30 22:05:28 +0530

    server component

---
+++ app/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,64 @@
+// "use client";
+import React from "react";
+import "./style.css";
+
+function ContactForm() {
+  return (
+    <form className="width100">
+      <div className="cInputs flex width100 spaceBtw">
+        <div className="width49">
+          <input placeholder="Name*" className="width100" />
+        </div>
+        <div className="width49">
+          <input placeholder="Email*" className="width100" />
+        </div>
+      </div>
+      <div className="cInputs flex width100 spaceBtw">
+        <div className="width49">
+          <input placeholder="Phone*" className="width100" />
+        </div>
+        <div className="width49">
+          <input placeholder="Website If Any" className="width100" />
+        </div>
+      </div>
+      <div className="cInputs flex width100 spaceBtw">
+        <div className="width49">
+          <input placeholder="Country" className="width100" />
+        </div>
+        <div className="width49">
+          <input placeholder="Company" className="width100" />
+        </div>
+      </div>
+      <div className="cInputs flex width100">
+        <div className="width100">
+          <select className="width100">
+            <option>Select Reason For Contacting</option>
+            <option>Services or pricing inquiry</option>
+            <option>Quote or proposal request</option>
+            <option>Technical assistance or support</option>
+            <option>Collaboration or partnership opportunities</option>
+            <option>Feedback or suggestions</option>
+            <option>Job or career inquiries</option>
+            <option>Invitations to speak or participate</option>
+            <option>Questions about qualifications or portfolio</option>
+            <option>Permission to use work or content</option>
+            <option>General inquiries or questions.</option>
+            <option>Other (Write It In Message)</option>
+          </select>
+        </div>
+      </div>
+      <div className="cInputs flex width100">
+        <div className="width100">
+          <textarea placeholder="Your Message" className="width100" rows="4" />
+        </div>
+      </div>
+      <div className=" flex width100">
+        <div className="width100">
+          <input type="submit" className="width100" />
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default ContactForm;
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -7,6 +7,7 @@ import {
   RiTwitterFill,
 } from "react-icons/ri";
 import Link from "next/link";
+import ContactForm from "../components/ContactForm/ContactForm";
 
 export const metadata = {
   title: "Contact - Harshit Kumar Full Stack Web Developer",
@@ -25,64 +26,7 @@ export default function Contact() {
       </section>
       <section className="contactContainer flex alignStart maxWidth spaceBtw">
         <div className="contactCLeft width49">
-          <form className="width100">
-            <div className="cInputs flex width100 spaceBtw">
-              <div className="width49">
-                <input placeholder="Name*" className="width100" />
-              </div>
-              <div className="width49">
-                <input placeholder="Email*" className="width100" />
-              </div>
-            </div>
-            <div className="cInputs flex width100 spaceBtw">
-              <div className="width49">
-                <input placeholder="Phone*" className="width100" />
-              </div>
-              <div className="width49">
-                <input placeholder="Website If Any" className="width100" />
-              </div>
-            </div>
-            <div className="cInputs flex width100 spaceBtw">
-              <div className="width49">
-                <input placeholder="Country" className="width100" />
-              </div>
-              <div className="width49">
-                <input placeholder="Company" className="width100" />
-              </div>
-            </div>
-            <div className="cInputs flex width100">
-              <div className="width100">
-                <select className="width100">
-                  <option>Select Reason For Contacting</option>
-                  <option>Services or pricing inquiry</option>
-                  <option>Quote or proposal request</option>
-                  <option>Technical assistance or support</option>
-                  <option>Collaboration or partnership opportunities</option>
-                  <option>Feedback or suggestions</option>
-                  <option>Job or career inquiries</option>
-                  <option>Invitations to speak or participate</option>
-                  <option>Questions about qualifications or portfolio</option>
-                  <option>Permission to use work or content</option>
-                  <option>General inquiries or questions.</option>
-                  <option>Other (Write It In Message)</option>
-                </select>
-              </div>
-            </div>
-            <div className="cInputs flex width100">
-              <div className="width100">
-                <textarea
-                  placeholder="Your Message"
-                  className="width100"
-                  rows="4"
-                />
-              </div>
-            </div>
-            <div className=" flex width100">
-              <div className="width100">
-                <input type="submit" className="width100" />
-              </div>
-            </div>
-          </form>
+          <ContactForm />
         </div>
         <div className="borderRight"></div>
         <div className="contactCRight width49 ">
