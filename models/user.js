89a84c288ca6560af5d9042afdb241491e6685d8
harshitclub <harshitclub@gmail.com>
2023-05-10 14:58:53 +0530

    remove message

---
+++ models/user.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  } else {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  }
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
