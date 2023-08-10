a7777b15b0dcb413fee251e58acc2f7e2f76835b
Harshit Kumar <harshitclub@gmail.com>
2023-08-10 17:53:26 +0530

    Update page.jsx

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -199,8 +199,8 @@ export default function About() {
         <h2>My Email</h2>
         <p>
           Mail:{" "}
-          <Link href="mailto:harshitclub@gmail.com">
-            harshitclub@gmail.com
+          <Link href="mailto:contact@harshitclub.com">
+           contact@harshitclub.com
             <RiExternalLinkLine className="aboutExperienceIcon" />
           </Link>
         </p>
